6ee694ebdedcdc3432a654882d91d903b69ea70e
Piotr Staworko <pio.staworko@gmail.com>
2025-03-23 19:13:17 +0100

    Initial Commit with unfinished project.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include "Philosopher.h"
+
+using namespace std;
+
+
+
+int main() {
+
+    int num_philosophers;
+    cout << "Enter the number of philosophers: ";
+    cin >> num_philosophers;
+
+    if (num_philosophers <= 0) {
+        cerr << "Invalid number of philosophers. Exiting." << endl;
+        return 1;
+    }
+
+    Philosopher dp(num_philosophers);
+    dp.start();
+
+    return 0;
+}
